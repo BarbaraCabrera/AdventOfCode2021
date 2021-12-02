cccc760758e5b75c11a0aea07daa78a891a50f4f
Alicia Garcia <alicia.garcia@liferay.com>
2021-12-02 12:28:20 +0100

    part 2

--- README.md
+++ README.md
@@ -14,11 +14,20 @@ To do this, count the number of times a depth measurement increases from the pre
 **How many measurements are larger than the previous measurement?**
 
 --- [Part Two](https://adventofcode.com/2021/day/1#part2https://adventofcode.com/2021/day/1#part2) ---
-
+<details>
+<summary>
+ part two
+</summary>
 Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
 
 Instead, consider sums of a three-measurement sliding window.
+
+![day1_part2.png](day1_part2.png)
+
 Start by comparing the first and second three-measurement windows.
+The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
 
-
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+Consider sums of a three-measurement sliding window. **How many sums are larger than the previous sum**?
+</details>
 </details>
--- src/main/java/com/liferay/adventofcode2021/Day1.java
+++ src/main/java/com/liferay/adventofcode2021/Day1.java
@@ -20,7 +20,10 @@ public class Day1 {
 			firstPartWithFor(Files.lines(file.toPath())));
 
 
-
+		System.out.println(
+			secondPartWithForIncreasing(Files.lines(file.toPath())));
+		System.out.println(
+			secondPartWithForDecreasing(Files.lines(file.toPath())));
 	}
 
 	private static String firstPartWithFor(Stream<String> lines) {
@@ -51,18 +54,33 @@ public class Day1 {
 		return counter;
 	}
 
-
-
-	private static String firstPartWithFor(Stream<String> lines) {
-		List<Integer> beeps = lines.map(Integer::parseInt).collect(Collectors.toList());
+	private static String secondPartWithForIncreasing(Stream<String> lines) {
+		List<Integer> beeps =
+			lines.map(Integer::parseInt).collect(Collectors.toList());
 
 		AtomicInteger counter = new AtomicInteger();
-int counter2 = 0;
-		//you must store all the list on memory
-		for(int i = 1; i < beeps.size(); i++){
-			if(beeps.get(i) > beeps.get(i-1)){
+
+		for (int i = 0; i < beeps.size() - 3; i++) {
+			int x = beeps.get(i) + beeps.get(i + 1) + beeps.get(i + 2);
+			int y = beeps.get(i + 1) + beeps.get(i + 2) + beeps.get(i + 3);
+			if (x < y) {
 				counter.getAndIncrement();
-				counter2++;
+			}
+		}
+		return String.valueOf(counter);
+	}
+
+	private static String secondPartWithForDecreasing(Stream<String> lines) {
+		List<Integer> beeps =
+			lines.map(Integer::parseInt).collect(Collectors.toList());
+
+		AtomicInteger counter = new AtomicInteger();
+
+		for (int i = 3; i < beeps.size(); i++) {
+			int x = beeps.get(i - 3) + beeps.get(i - 2) + beeps.get(i - 1);
+			int y = beeps.get(i - 2) + beeps.get(i - 1) + beeps.get(i);
+			if (x < y) {
+				counter.getAndIncrement();
 			}
 		}
 		return String.valueOf(counter);
